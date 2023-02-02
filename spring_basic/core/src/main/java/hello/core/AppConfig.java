4932f4eb8d5b2ce3e93035d834277d6555530c8c
lign25 <geonwoo.dev@gmail.com>
2023-02-03 03:24:22 +0900

    AppConfig와 분리

--- spring_basic/core/src/main/java/hello/core/AppConfig.java
+++ spring_basic/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,14 +12,26 @@ import hello.core.order.OrderServiceImpl;
 
 //앱 실제 동작에 필요한 구현객체를 생성, 객체의 생성과 연결을 AppConfig가 담당.
 //설계 변경으로 인해 DIP를 지키는 설계방식
+
 public class AppConfig {
 
+    //역할을 세우고 구현이 내부에 들어가는 구조
     public MemberService memberService() {
-        //생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //OCP,DIP 원칙을 지킴
+    //정액할인, 정률할인 변경시 DiscountPolicy만 수정하여 변경가능
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }

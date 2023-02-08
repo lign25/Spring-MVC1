f96476660d88fb07e82f88c654b71410ff680306
lign25 <geonwoo.dev@gmail.com>
2023-02-09 05:03:37 +0900

    빈 생명주기 콜백

---
+++ spring_basic/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,34 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.Lifecycle;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    //스프링 빈은 객체생 -> 의존관계 주입 라이프사이클을 가진다.
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+
+
+}

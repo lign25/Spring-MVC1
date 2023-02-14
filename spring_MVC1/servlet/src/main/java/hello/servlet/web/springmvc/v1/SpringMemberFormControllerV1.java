f18a75ffaebd46a7a0391d1dfe3392c7465c0728
lign25 <geonwoo.dev@gmail.com>
2023-02-14 17:30:35 +0900

    스프링MVC - 구조 이해

---
+++ spring_MVC1/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//@Controller 내부에 @Component가 포함되어 있어 자동으로 빈에 등록됨
+@Controller
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
